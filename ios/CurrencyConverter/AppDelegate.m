1c8e7b9ff2e374256502ba7719db8935e46c4d17
Abhay Garg <abhaydgarg@gmail.com>
2018-07-16 10:31:17 +0530

    Updated package.json and configure ios release for physical device

--- ios/CurrencyConverter/AppDelegate.m
+++ ios/CurrencyConverter/AppDelegate.m
@@ -16,7 +16,13 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  #ifdef DEBUG
+    // DEV
+    jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  #else
+  // PROD
+    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  #endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"CurrencyConverter"
